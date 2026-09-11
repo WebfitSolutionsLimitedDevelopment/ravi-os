3402abac1aa94d70b443680397a2f802812b1d96
WebfitSolutionsLimitedDevelopment <srikantnz022024@gmail.com>
2026-09-11 22:45:45 +1200

    feat(journal): wire journal navigation globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import LockGate from '../components/lock-gate'
 import TaskNavigationBridge from '../components/task-navigation-bridge'
 import HomePolishBridge from '../components/home-polish-bridge'
 import FamilyNavigationBridge from '../components/family-navigation-bridge'
+import JournalNavigationBridge from '../components/journal-navigation-bridge'
 
 export const metadata = {
   title: 'Ravi OS',
@@ -15,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body><LockGate>{children}</LockGate><TaskNavigationBridge/><HomePolishBridge/><FamilyNavigationBridge/></body>
+      <body><LockGate>{children}</LockGate><TaskNavigationBridge/><HomePolishBridge/><FamilyNavigationBridge/><JournalNavigationBridge/></body>
     </html>
   )
 }
